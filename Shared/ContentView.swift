3c976e4b6b59ef509e214eab57263333de7da531
Stefan Ruzic <30556980+rule1211@users.noreply.github.com>
2021-09-26 12:11:07 +0200

    Update ContentView.swift
    URl

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,16 +7,56 @@
 
 import SwiftUI
 
-// https://random.imagecdn.app/500.500
 
+class ViewModel: ObservableObject {
+    @Published var image: Image?
+    func fetchNewImage() {
+        guard let url = URL(string: "https://random.imagecdn.app/500.500")  else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) {
+            data, _, _ in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else {
+                    return
+                }
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+        task.resume()
+    }
+    
+}
 struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
+                
                 Image(systemName: "photo")
                     .resizable()
                     .foregroundColor(Color.pink)
-                    .frame(width: 300, height: 300)
+                    .frame(width: 200, height: 200)
+                    .padding()
+                
+                Spacer()
+                
+                Button(action: {
+                    // do something
+                }, label: {
+                    Text("New Image!")
+                        .bold()
+                        .frame(width: 250, height: 55)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding()
+                })
+
                 
             }
             .navigationTitle("Photo Mania")
