5ee2df198f88b4d9cf12b1ceac7869c39da2f071
Stefan Ruzic <30556980+rule1211@users.noreply.github.com>
2021-09-26 12:28:56 +0200

    Update ContentView.swift
    image and background

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,11 +11,10 @@ import SwiftUI
 class ViewModel: ObservableObject {
     @Published var image: Image?
     func fetchNewImage() {
-        guard let url = URL(string: "https://random.imagecdn.app/500.500")  else {
+        guard let url = URL(string: "https://random.imagecdn.app/500/500")  else {
             return
         }
-        let task = URLSession.shared.dataTask(with: url) {
-            data, _, _ in
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
             guard let data = data else { return }
             DispatchQueue.main.async {
                 guard let uiImage = UIImage(data: data) else {
@@ -37,16 +36,31 @@ struct ContentView: View {
             VStack {
                 Spacer()
                 
+                if let image = viewModel.image {
+                    ZStack {
+                    image
+                        .resizable()
+                        .foregroundColor(Color.pink)
+                        .frame(width: 200, height: 200)
+                        .padding()
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 1.2, height: UIScreen.main.bounds.width / 1.2)
+                    .background(Color.green)
+                    .cornerRadius(8)
+                } else {
                 Image(systemName: "photo")
                     .resizable()
                     .foregroundColor(Color.pink)
                     .frame(width: 200, height: 200)
                     .padding()
+                }
                 
                 Spacer()
                 
                 Button(action: {
-                    // do something
+                    
+                    viewModel.fetchNewImage()
+                    
                 }, label: {
                     Text("New Image!")
                         .bold()
