cd543d770ae41fcf6265f2b6f9d726a10a45a799
Stefan Ruzic <30556980+rule1211@users.noreply.github.com>
2021-09-26 11:16:23 +0200

    Initial Commit

---
+++ Shared/PhotomaniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotomaniaApp.swift
+//  Shared
+//
+//  Created by Stefan Ruzic on 26.9.21..
+//
+
+import SwiftUI
+
+@main
+struct PhotomaniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
